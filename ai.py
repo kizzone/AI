2853101649df209854f207e6bdde9ab1acccdb54
kizzone <kizzone.acm@gmail.com>
2019-03-16 10:57:35 +0100

    impostato il crossover tra i genitori migliori

--- ai.py
+++ ai.py
@@ -5,24 +5,23 @@ from keras.utils import np_utils
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, InputLayer
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
-from keras.layers import InputLayer #per specificare l'input shape
 import os
 import random
 import time
 from numpy.random import choice
+from keras import backend as K
+K.set_image_dim_ordering('th')
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
 batch_size = 32
 num_classes = 10
 epochs = 20
-populationSize = 50
+populationSize = 10
 generation = 100
 #----------------------------------------------------------------------------------------------------------------------------------------------
-
 #TODO inserire un seed per riprodurre l'esperimento
 def createPseudoRandomGenotype():
     
@@ -64,8 +63,6 @@ def createPseudoRandomGenotype():
     print ( "Randome genes :"  + str (randomGenes))
     
     return randomGenes
-
-#----------------------------------------------------------------------------------------------------------------------------------------------
 def generateRandomActivation():
     flag = bool(random.getrandbits(1))
     activationType = choice(["relu" , "elu" , "tanh" ], p=[0.6, 0.3, 0.1])
@@ -98,9 +95,8 @@ def generateRandomMaxPool(depth):
     if depth < 2:
         size = choice([2 , 4 ], p=[0.6, 0.4])
     else:
-        size = choice([2 , 4  ], p=[0.4, 0.6])
+        size = choice([2 , 4  ], p=[0.8, 0.2])
     return flag, size
-
 
 '''
 L'individuo è caratterizzato da un genotipo e la sua accuratezza
@@ -111,7 +107,7 @@ class Individual (object):
         self.accuracy = accuracy
     
     def __repr__(self):
-        return "Hi my genotype is \n" + str(self.genotype) + "\nmy accuracy is:  " + str(self.accuracy) + "\n"
+        return str(self.genotype) + ":" + str(self.accuracy) + "\n"
 
 def createModelFromGenotype( genoma ,x_train):
     
@@ -125,88 +121,125 @@ def createModelFromGenotype( genoma ,x_train):
     beforeFlatten = []
     afterFlatten = []
 
-    for j in range (0,5):
-        beforeFlatten.append( part1[9 * j: (9*j) + 9 ])
+
+    try: 
+        for j in range (0,5):
+            beforeFlatten.append( part1[9 * j: (9*j) + 9 ])
+        
+        for row in beforeFlatten:
+            for i in range (0,3):
+                if row[0] == True:
+                    #print("Creo una convoluzione con numero di filtri uguale a " + str(row[1]) + "e kernel di dimensione" + str(row[2]))
+                    model.add(Conv2D( row[1] , (row[2], row[2]), padding='same' ) )
+                    break
+            for i in range (3,5):
+                if row[3] == True:
+                    #print("Creo un Activation con tipo " + str(row[4] ))
+                    model.add( Activation(row[4]))
+                    break
+            for i in range (5,7):
+                if row[5] == True:
+                    #print("Creo un Maxpooling con  dimensione max " + str( row[6] ) )
+                    model.add( MaxPooling2D ( pool_size = (row[6], row[6]), data_format='channels_first' ) )
+                    break
+            for i in range (7,9):
+                if row[7] == True:
+                    #print("Creo un Dropout  con  rate : " + str( row[8] ) )
+                    model.add( Dropout (row[8] )  )
+                    break
+        
+        for j in range (0,3):
+            afterFlatten.append( part2[6 * j: (6*j) + 6 ])
     
-    for row in beforeFlatten:
-        for i in range (0,3):
-            if row[0] == True:
-                #print("Creo una convoluzione con numero di filtri uguale a " + str(row[1]) + "e kernel di dimensione" + str(row[2]))
-                model.add(Conv2D( row[1] , (row[2], row[2]), padding='same' ) )
-                break
-        for i in range (3,5):
-            if row[3] == True:
-                #print("Creo un Activation con tipo " + str(row[4] ))
-                model.add( Activation(row[4]))
-                break
-        for i in range (5,7):
-            if row[5] == True:
-                #print("Creo un Maxpooling con  dimensione max " + str( row[6] ) )
-                model.add( MaxPooling2D ( pool_size = (row[6], row[6]) ) )
-                break
-        for i in range (7,9):
-             if row[7] == True:
-                #print("Creo un Dropout  con  rate : " + str( row[8] ) )
-                model.add( Dropout (row[8] )  )
-                break
-    
-    for j in range (0,3):
-        afterFlatten.append( part2[6 * j: (6*j) + 6 ])
-   
-    #----------------------------------------------------layer fisso-----------------------------------------------
-    model.add(Flatten())
+        #----------------------------------------------------layer fisso-----------------------------------------------
+        model.add(Flatten())
 
-    for row in afterFlatten:
-        for i in range (0,2):
-            if row[0] == True:
-                #print("Creo un Dense  con neuroni " + str( row[1] ) )
-                model.add(Dense(row[1]))
-                break
-        for i in range (2,4):
-            if row[2] == True:
-                #print("Creo un Activation con tipo " + str(row[3] ))
-                model.add( Activation(row[3]))
-                break
-        for i in range (4,6):
-            if row[4] == True:
-                #print("Creo un Dropout  con  rate : " + str( row[5] ) )
-                model.add( Dropout (row[5] )  )
-                break
+        for row in afterFlatten:
+            for i in range (0,2):
+                if row[0] == True:
+                    #print("Creo un Dense  con neuroni " + str( row[1] ) )
+                    model.add(Dense(row[1]))
+                    break
+            for i in range (2,4):
+                if row[2] == True:
+                    #print("Creo un Activation con tipo " + str(row[3] ))
+                    model.add( Activation(row[3]))
+                    break
+            for i in range (4,6):
+                if row[4] == True:
+                    #print("Creo un Dropout  con  rate : " + str( row[5] ) )
+                    model.add( Dropout (row[5] )  )
+                    break
 
-    #----------------------------------------------------layers fissi----------------------------------------------- 
-    model.add(Dense(num_classes))
-    model.add(Activation('softmax'))
+        #----------------------------------------------------layers fissi----------------------------------------------- 
+        model.add(Dense(num_classes))
+        model.add(Activation('softmax'))
 
-    model.summary()
-    return model
+        model.summary()
+        return model
+    except:
+        return None
 
 def createAndEvaluate(genome, x_train, x_test, y_train,y_test):
 
-    #genoma = [True, 64, 3, True, 'relu', True, 2, True, 0.25, True, 32, 5, True, 'relu', True, 2, True, 0.3, True, 32, 3, True, 'relu', True, 2, True, 0.4, True, 64, 3, True, 'relu', True, 2, True, 0.25, True, 32, 5, True, 'relu', True, 2, True, 0.4, True, 64, True, 'relu', True, 0.25, True, 64, True, 'relu', True, 0.3, True, 64, True, 'relu', True, 0.4]
-    
-
     model = createModelFromGenotype (genome,x_train)
 
-    try:
-        # initiate RMSprop optimizer
-        opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-        # Let's train the model using RMSprop
-        model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
-        earlystop = keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.0, patience=3, verbose=1, mode='auto')
-        callbacks_list = [earlystop]
+    if model is not None:
+        try:
+            # initiate RMSprop optimizer
+            opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+            # Let's train the model using RMSprop
+            model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
+            earlystop = keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.0, patience=3, verbose=1, mode='auto')
+            callbacks_list = [earlystop]
 
-        x_train = x_train.astype('float32')
-        x_test = x_test.astype('float32')
-        x_train /= 255
-        x_test /= 255
+            x_train = x_train.astype('float32')
+            x_test = x_test.astype('float32')
+            x_train /= 255
+            x_test /= 255
 
-        model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,validation_data=(x_test, y_test),shuffle=True,verbose=1,callbacks=callbacks_list)
+            model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,validation_data=(x_test, y_test),shuffle=True,verbose=1,callbacks=callbacks_list)
 
-        scores = model.evaluate(x_test, y_test, verbose=1)
-        return scores[1]
+            scores = model.evaluate(x_test, y_test, verbose=1)
+            return scores[1]
 
-    except:
+        except:
+            return -1
+    else:
         return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -222,8 +255,7 @@ def main():
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
     population = []
-
-    
+    currentpopulation = []
 
     start = time.time()
 
@@ -234,9 +266,7 @@ def main():
         testSub = Individual(genoma,accuracy)
         population.append(testSub)
 
-    #stampa i risultati
-    #for obj in population:
-    ##    print(str(obj))
+
     end = time.time()
 
     elapsedTime = end - start
@@ -246,6 +276,8 @@ def main():
 
     #do ngeneration times
     #create a new population from the "best" ones and run new evaluation
+    #for i in range (0, generation):
+
 
 if __name__ == "__main__":
     main()
--- prova.py
+++ prova.py
@@ -1,4 +1,149 @@
+from __future__ import print_function
+from ai import createModelFromGenotype
 
-from tensorflow.python.client import device_lib
+import numpy as np
+import keras
+from keras.utils import np_utils
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from keras.layers import InputLayer #per specificare l'input shape
+import os
+from random import randint,uniform
+import time
+from numpy.random import choice
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+
+batch_size = 32
+num_classes = 10
+epochs = 20
+populationSize = 10
+generation = 100
+
+(x_train1, y_train1), (x_test1, y_test1) = cifar10.load_data()
+#Mi prendo il 10% del training
+x_train, X_test_scartare, y_train, y_test_scartare = train_test_split(x_train1, y_train1, train_size=0.40, random_state=42, stratify=y_train1)
+#Mi prendo il 10% del test
+X_train_scartare, x_test, y_train_scartare, y_test = train_test_split(x_test1, y_test1, test_size=0.40, random_state=42, stratify=y_test1)
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+
+
+class Individual(object):
+    def __init__(self, genotype, accuracy):
+        self.genotype = genotype
+        self.accuracy = accuracy
     
-print(device_lib.list_local_devices())
+    def __repr__(self):
+        return str(self.genotype) + ":" + str(self.accuracy) + "\n"
+
+
+
+list1 =  [False, 32, 7, False, 'relu', False, 2, False, 0.4, False, 32, 3, False, 'elu', True, 4, True, 0.4, True, 16, 7, False, 'elu', True, 4, False, 0.5, True, 64, 7, True, 'elu', True, 4, False, 0.3, False, 32, 7, False, 'elu', True, 4, False, 0.4, True, 128, True, 'tanh', True, 0.3, False, 64, False, 'elu', False, 0.3, True, 32, True, 'elu', True, 0.4]
+list2 = [False, 32, 7, False, 'relu', True, 2, False, 0.4, False, 32, 3, False, 'elu', True, 4, True, 0.2, False, 16, 7, False, 'elu', True, 4, False, 0.5, True, 32, 7, True, 'relu', True, 2, True, 0.3, False, 16, 7, False, 'relu', True, 4, False, 0.2, False, 128, True, 'tanh', True, 0.3, False, 32, False, 'elu', True, 0.3, True, 32, False, 'relu', True, 0.2]
+list3 = [True, 32, 7, False, 'elu', False, 2, False, 0.4, False, 32, 3, True, 'elu', True, 2, True, 0.4, True, 16, 7, False, 'elu', True, 4, False, 0.5, True, 64, 7, True, 'elu', True, 2, True, 0.3, False, 16, 7, False, 'relu', True, 4, False, 0.2, False, 128, True, 'tanh', True, 0.3, False, 32, False, 'elu', True, 0.3, True, 32, False, 'relu', True, 0.2]
+list4 = [False, 32, 7, False, 'relu', True, 2, False, 0.4, False, 32, 3, False, 'relu', True, 2, True, 0.2, True, 16, 7, False, 'elu', True, 4, False, 0.5, False, 64, 7, True, 'elu', True, 2, True, 0.3, False, 16, 7, False, 'tanh', False, 4, False, 0.2, False, 128, True, 'tanh', False, 0.4, True, 32, False, 'elu', True, 0.3, True, 32, False, 'relu', True, 0.2]
+list5 =[True, 32, 7, False, 'elu', False, 2, False, 0.4, False, 32, 3, True, 'elu', True, 2, True, 0.4, True, 16, 7, False, 'elu', True, 4, False, 0.5, False, 64, 7, True, 'elu', True, 2, True, 0.3, False, 16, 7, False, 'elu', True, 4, False, 0.4, True, 128, True, 'tanh', True, 0.3, False, 64, False, 'elu', False, 0.3, True, 32, True, 'elu', True, 0.4]
+
+individuo = Individual (list1,0.8)
+individuo2 = Individual (list2,0.6)
+individuo3= Individual (list3,0.2)
+individuo4 = Individual (list4,0.9)
+individuo5 = Individual (list5,0.1)
+
+olderGeneration = []
+olderGeneration.append(individuo)
+olderGeneration.append(individuo2)
+olderGeneration.append(individuo3)
+olderGeneration.append(individuo4)
+olderGeneration.append(individuo5)
+
+
+#--------------------------------------------------------------------------------
+def selecteTwoRandowPeople (list):
+    rand1 = randint (0, len(list)-1)
+    rand2 = randint (0, len(list)-1)
+    if rand2 != rand1:
+        return list[rand1],list[rand2]
+    else:
+        return selecteTwoRandowPeople(list)
+
+def combine(a,b):
+
+    c = []
+    split = randint (0, len(b)) 
+    c.extend ( a[:split])
+    c.extend ( b[split:])
+    #making a random mutation at a given rate
+    return c
+
+def crossoverAndMutation(lista):
+        
+    newGenotypes = []
+    for i in range (0, populationSize):
+        a,b = selecteTwoRandowPeople(lista)
+        #print ( "selected a and b " + str(a) + "\n\n" + str(b))
+        c = combine(a,b)
+        #print("RESULTING COMBINATION IS " + str(c))
+        newGenotypes.append(c)
+        #print("newGenotype is now  " + str(newGenotypes))
+    return newGenotypes
+
+def binarySearch(data, val):
+    lo, hi = 0, len(data) - 1
+    best_ind = lo
+    while lo <= hi:
+        mid2 = lo + (hi - lo) / 2
+        mid = int(mid2)
+        if data[mid] < val:
+            lo = mid + 1
+        elif data[mid] > val:
+            hi = mid - 1
+        else:
+            best_ind = mid
+            break
+        # check if data[mid] is closer to val than data[best_ind] 
+        if abs(data[mid] - val) < abs(data[best_ind] - val):
+            best_ind = mid
+    return best_ind
+
+def createNewGeneration(olderGeneration):
+
+    #selecting individal with fitness above average
+    newerGenerationGenotype = []
+    toMix = []
+
+    olderGenerationaccuracy = [ obj.accuracy for obj in olderGeneration ]
+    weigthedprobability = np.cumsum( olderGenerationaccuracy)
+
+    for i in range (0, populationSize):
+        #contare pure i -1??
+        rand = uniform (0, weigthedprobability[-1] )
+        #trovare a quale indice appartiene e ritornare il cromosoma corrispondente a quell'accurac
+        index = binarySearch(weigthedprobability, rand)
+
+        cromosoma =  [ t.genotype for t in olderGeneration if t.accuracy ==  olderGenerationaccuracy[index] ]
+    
+        toMix.extend(cromosoma)
+        
+    #print (" Selected nodes to MIX:" + str ( toMix ) )
+    x = crossoverAndMutation( toMix )
+    newerGenerationGenotype.extend( x )
+
+    return newerGenerationGenotype
+
+#--------------------------------------------------------------------------------
+
+p = createNewGeneration(olderGeneration)
+
+for i in range (0, len(p)):
+    print ( "index " + str(i) + " is " + str(p[i])  + "\n" )
